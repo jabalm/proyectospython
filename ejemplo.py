1282ed804ebfbc405fc31a1fd43f66ff88fca9ce
jabalm <82182552+jabalm@users.noreply.github.com>
2021-04-08 16:39:11 -0500

    Add files via upload

---
+++ ejemplo.py
@@ -0,0 +1,15 @@
+#datos de entrada
+a = float(input("Ingrese primer numero: "))
+b = float(input("Ingrese segundo numero: "))
+#proceso
+s = a+b
+r = a-b
+m = a*b
+d = a/b
+rd = a%b
+#salida de datos
+print("La Suma es: ",s)
+print("La Resta es: ",r)
+print("La multiplicacion es: ",m)
+print("La Division es: ",d)
+print("Resto de Division: ", rd)
